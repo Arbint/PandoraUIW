51fa0daf08805eabaf8059a3373f8f1328f088c0
Li <jili1@uiwtx.edu>
2023-11-17 12:12:11 -0600

    now trying to setup maya plug-ins

--- PandoraAutoConfig/main.py
+++ PandoraAutoConfig/main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import os.path
+import sys
+import subprocess
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PySide6.QtCore import QStandardPaths
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, QFileDialog, QLineEdit, QLabel
+from PySide6 import QtCore
+from UserInterfacesPandora import qdarkstyle
+
+class PandoraInstallerWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("UIW Pandora Auto Installer 0.0.1")
+
+        #Widgets
+        self.maya_path_hint = QLabel("Maya Document Path should be C:\\Users\\your_user_name\\Documents\\maya\\maya_version")
+        self.maya_path_label = QLabel("Maya Document Path: ")
+        self.maya_path_field = QLineEdit()
+        self.load_path_button = QPushButton("...")
+        self.install_button = QPushButton("Install")
+
+        #layout
+        self.layout = QVBoxLayout(self)
+        self.path_layout = QHBoxLayout(self)
+
+        self.layout.addWidget(self.maya_path_hint)
+
+        self.layout.addLayout(self.path_layout)
+        self.path_layout.addWidget(self.maya_path_label)
+        self.path_layout.addWidget(self.maya_path_field)
+        self.path_layout.addWidget(self.load_path_button)
+
+        self.layout.addWidget(self.install_button)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        #deault values:
+        documents_path = QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation)
+        self.maya_path_field.setText(documents_path + "/maya/2024")
+
+        #functionality
+        self.load_path_button.clicked.connect(self.pick_maya_path)
+        self.install_button.clicked.connect(self.install)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        #style
+        self.resize(600, 100)
+        self.setStyleSheet(qdarkstyle.load_stylesheet(pyside=True))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+    def pick_maya_path(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ShowDirsOnly | QFileDialog.DontUseNativeDialog
+
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory", options=options)
+
+        if directory:
+            self.maya_path_field.setText(directory)
+
+    def install(self):
+        exec_dir = os.path.dirname(os.path.abspath(__file__))
+        parent_dir = os.path.dirname(os.path.abspath(exec_dir))
+        os.chdir(parent_dir)
+        vendor_installer_name = "Pandora_Setup.bat"
+        vendor_installer_path = os.path.join(parent_dir, vendor_installer_name)
+        print(vendor_installer_path)
+        subprocess.run([vendor_installer_path])
+
+if __name__ == "__main__":
+    app = QApplication([])
+    widget = PandoraInstallerWidget()
+    widget.show()
+
+    sys.exit(app.exec())
